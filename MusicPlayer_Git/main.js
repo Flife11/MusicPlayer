e17cd3079ef8851b0bc0e84417ff29516c8109ab
Flife11 <lesangbt99@gmail.com>
2021-10-12 07:47:18 +0700

    Add some events

--- MusicPlayer_Git/main.js
+++ MusicPlayer_Git/main.js
@@ -10,35 +10,43 @@ const $$ = document.querySelectorAll.bind(document);
 
 function Run(data) { const app =
 {
+    loopState: false,
     playState: false,
     index: 0,
     songs: data,
 
     createPlaylist: function(songs) {
-        const playlist = songs.map(song => {
-            return `<div class="song">
-                        <div class="song-thumbnail" style="background-image: url(${song.image})"></div>
-                        <div class="description">
-                            <h3 class="song-name">${song.name}</h3>
-                            <h5 class="author">${song.author}</h5>
-                        </div>
-                        <div class="option"><i class="bi bi-three-dots"></i></div>
-                    </div>`;
+        var playlist = $('.playlist');
+        songs.forEach(song => {
+            const html = `  <div class="song-thumbnail" style="background-image: url(${song.image})"></div>
+                            <div class="description">
+                                <h3 class="song-name">${song.name}</h3>
+                                <h5 class="author">${song.author}</h5>
+                            </div>
+                            <div class="option"><i class="bi bi-three-dots"></i></div>` 
+            var div = document.createElement("div");
+            div.className = `${song.id} song`;
+            div.innerHTML = html;
+            playlist.appendChild(div);
         })
-        $('.playlist').innerHTML = playlist.join("");
     },
 
     loadCurrentSong: function() {
+        const playlist = $$(".song");
+        var active = $(".song.active");
         var header = $(".header>h2");
         var thumbnail = $(".cd-thumbnail");
         var audio = $("audio");
         header.innerHTML = `${this.songs[this.index].name}`;
         thumbnail.style.backgroundImage = `url(${this.songs[this.index].image})`;
         audio.src = `${this.songs[this.index].audio}`;
+        if(active) active.classList.remove("active");
+        playlist[this.index].classList.add("active");
     },
 
     handelerEvent: function() {
         // Shrink thumbnail onscroll
+
         const cd = $(".cd");
         const offsetWidth = cd.offsetWidth;
 
@@ -55,21 +63,78 @@ function Run(data) { const app =
         pause.onclick = function() {
             if (app.playState) {
                 audio.pause();
-                pause.children[0].style.display = "none";
-                pause.children[1].style.display = "initial";
+                pause.children[0].classList.add("paused");
+                pause.children[1].classList.remove("paused");
             } else {
                 audio.play();
-                pause.children[1].style.display = "none";
-                pause.children[0].style.display = "initial";
+                pause.children[0].classList.remove("paused");
+                pause.children[1].classList.add("paused");
             }
             app.playState = !app.playState;
         }
 
-        //
+        // Progress bar
+        const input = $("#progress");
+
+        input.onchange = function(e) {
+            audio.currentTime = (audio.duration*e.target.value)/100;
+            // console.log(audio.currentTime);
+        }
+        audio.ontimeupdate = function() {
+            if (audio.duration) input.value = (audio.currentTime*100)/audio.duration;
+            // console.log(input.value);
+        }
+
+        // Next, Previous events
+        const next = $(".next");
+        const previous = $(".previous");
+
+        next.onclick = function() {
+            app.index = (app.index+1)%(app.songs.length);
+            app.loadCurrentSong();
+            app.playState = 0;
+            pause.onclick();
+        }
+        previous.onclick = function() {
+            app.index = (app.index-1+app.songs.length)%(app.songs.length);
+            app.loadCurrentSong();
+            app.playState = 0;
+            pause.onclick();
+        }
+        audio.onended = function() {
+            next.onclick();
+        }
+
+        // Loop event
+        const loop = $(".loop")
+
+        loop.onclick = function() {
+            if (app.loopState) {
+                loop.classList.remove("loopping");
+                audio.loop = false;
+            } else {
+                loop.classList.add("loopping");
+                audio.loop = true;
+            }
+            app.loopState = !app.loopState
+        }
+
+        // Pick song event
+        var playlist = $$(".song");
+        playlist.forEach(song => {
+            song.onclick = function() {
+                const index = song.classList[0];
+                app.index = index-1;
+                app.loadCurrentSong();
+                app.playState = 0;
+                pause.onclick();
+            }
+        })
     },
 
     start: function() {
         // Create playlist
+        
         this.createPlaylist(this.songs);
 
         this.loadCurrentSong();
